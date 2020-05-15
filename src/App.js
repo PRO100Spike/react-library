8b0bde6f15964799bd5732eb171699d3029c6f38
Yakushew Anton <spike2work@gmail.com>
2020-05-15 22:41:51 +0300

    add header del react init app page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
+
+import Header from './layout/Header'
 import About from "./view/About";
 import Contact from "./view/Contact";
 import Home from "./view/Home";
 import Library from "./view/Library";
-import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Привет реакт!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
         <nav>
           <ul>
             <li>
---
+++ src/layout/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import '../assets/css/header.css';
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+const Header = () => {
+  return (
+    <div class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Header
